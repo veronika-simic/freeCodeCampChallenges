e8f684f24abedeadf5c0b232e3406497246b0c14
veronika-simic <veronikasimic21@gmail.com>
2022-07-11 13:20:55 +0200

    Add js for adding and removing cap

--- script.js
+++ script.js
@@ -2,6 +2,13 @@ const caps = document.querySelectorAll('.cap');
 console.log(caps);
 
 caps.forEach(cap => {cap.addEventListener('click', ()=>{
-    cap.style.left = '-100px';
+    if (cap.classList.contains('left')){
+        cap.classList.remove('left');
+    } else {
+        cap.classList.add('left');
+    }
+   
 })})
+
+
 
