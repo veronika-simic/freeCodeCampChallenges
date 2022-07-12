990f8507214d037fdbdedac8fafc933a4260a43c
veronika-simic <veronikasimic21@gmail.com>
2022-07-12 10:55:06 +0200

    Add variables for width and height of canvas

--- script.js
+++ script.js
@@ -12,8 +12,13 @@ caps.forEach(cap => {cap.addEventListener('click', ()=>{
 
 
 /* canvas */
-const canvas = document.querySelector('.canvas');
-let ctx = canvas.getContext('2d');
+const canvas = document.querySelector('canvas');
+const ctx = canvas.getContext('2d');
+const canvasOffsetX = canvas.offsetLeft; /* distance from the left */
+const canvasOffsetY = canvas.offsetTop;  /* distance from top */
+canvas.width = window.innerWidth - canvasOffsetX; /* width of canvas */
+canvas.height = window.innerHeight - canvasOffsetY; /* height of canvas */
+
 let isDrawing = false /* is user drawing */
 let lineWidth = 5; /* line width 5px */
 let startX;
